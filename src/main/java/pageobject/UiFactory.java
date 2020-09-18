67a710fca669f4b3dc5a74371420eb013ab4ff26
Johncoo682 <johncoo682@gmail.com>
2020-09-18 17:28:12 +0530

    Second Commit 1

--- src/main/java/pageobject/UiFactory.java
+++ src/main/java/pageobject/UiFactory.java
@@ -1,7 +1,7 @@
 package pageobject;
 
 public class UiFactory {
-	public static final String Check = "Adi";
+	public static final String Check = "Aditya";
 	public static final String s="Gobinda with new version";
 	public static final String newString="anothercommit";
 	
