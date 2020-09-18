35941f2c3276da74f94793d42d54371b9a26af94
Johncoo682 <johncoo682@gmail.com>
2020-09-18 17:08:59 +0530

    John Cook has made one changes

--- src/main/java/pageobject/UiFactory.java
+++ src/main/java/pageobject/UiFactory.java
@@ -2,11 +2,16 @@ package pageobject;
 
 public class UiFactory {
 	public static final String Check = "Adi";
-	public static final String s="John.0";
+	public static final String s="Gobinda Changrs";
+	
+	public void testSetup() {
+		
+		
+	}
 	public static final String q="Wow";
-
+	
 	public void doJib() {
-
+ 
 		System.out.println();
 	}
 
