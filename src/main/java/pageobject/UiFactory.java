f1f317882c6707447e84e30ca0c3bf5283663056
Gobinda Roy <Gobinda.Roy0406@gmail.com>
2020-09-18 17:31:20 +0530

    Changes

--- src/main/java/pageobject/UiFactory.java
+++ src/main/java/pageobject/UiFactory.java
@@ -1,19 +1,24 @@
 package pageobject;
 
 public class UiFactory {
-	public static final String Check = "Aditya";
+
 	public static final String s="Gobinda with new version";
 	public static final String newString="anothercommit";
-	
+
+	public static final String Check = "NewAditya";
+
 	public void testSetup() {
-		
-		
+
 	}
 
-	public static final String q="Wow";
-	
+	public void tearDown() {
+
+	}
+
+	public static final String q = "Wow";
+
 	public void doJib() {
- 
+
 		System.out.println();
 	}
 
