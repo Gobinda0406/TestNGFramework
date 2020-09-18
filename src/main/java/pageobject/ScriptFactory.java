23d28b119b3f962f101c9561a0923e5435737f26
goldebazar <goldebazar@gmail.com>
2020-09-18 16:06:56 +0530

    John Commit 1

---
+++ src/main/java/pageobject/ScriptFactory.java
@@ -0,0 +1,10 @@
+package pageobject;
+
+public class ScriptFactory {
+
+	public void login(String name) {
+	for (int i = 0; i < 10; i++) {
+		
+	}
+	}
+}
--- src/main/java/pageobject/UiFactory.java
+++ src/main/java/pageobject/UiFactory.java
@@ -2,4 +2,7 @@ package pageobject;
 
 public class UiFactory {
 
+	//This is my code
+	public static final String s="John.0";
+	public static final String q="Wow";
 }
