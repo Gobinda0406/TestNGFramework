555f31e46c11c1929f331960b246062c26fb4338
Gobinda Roy <Gobinda.Roy0406@gmail.com>
2021-01-17 23:19:40 +0530

    master 3 commit

--- src/main/java/newFeature/NewCommit.java
+++ src/main/java/newFeature/NewCommit.java
@@ -1,11 +1,14 @@
 package newFeature;
 
 public class NewCommit {
-int j;
-public void test1(){
+int k;
+public void test11(){
 	System.out.println("I am in test1");
 }
 public void test2(){
 	System.out.println("I am in test2");
+}
+public void test3(){
+	
 }
 }
