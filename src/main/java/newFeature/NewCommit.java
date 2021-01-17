565a5f32f20482e4f29902a03d818329c3538673
Gobinda Roy <Gobinda.Roy0406@gmail.com>
2021-01-17 23:25:40 +0530

    feature4 commit 1

--- src/main/java/newFeature/NewCommit.java
+++ src/main/java/newFeature/NewCommit.java
@@ -1,11 +1,18 @@
 package newFeature;
 
 public class NewCommit {
-int j;
+int m;
 public void test1(){
 	System.out.println("I am in test1");
 }
 public void test2(){
 	System.out.println("I am in test2");
+	System.out.println("I am in test2 twice");
+}
+public void test3(){
+	System.out.println("I am in test3");
+}
+public void newFeature4(){
+	
 }
 }
