eadd1588bf082c098f308495bcb5d0bdfeef0cef
Gobinda Roy <Gobinda.Roy0406@gmail.com>
2021-01-17 13:24:23 +0530

    m1 commit

--- src/main/java/newFeature/CartFeature.java
+++ src/main/java/newFeature/CartFeature.java
@@ -13,4 +13,13 @@ public class CartFeature {
 	public void doe(){
 		System.out.println("Test");
 	}
+	int x;
+	public void master1(int x){
+		System.out.println(x);
+	}
+	
+	int y,z;
+	public void check(int y, int z){
+		System.out.println(y+z);
+	}
 }
